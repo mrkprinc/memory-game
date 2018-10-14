45c4ed156113f3d47d7d48e88e8bf08669a494af
mrkprinc <36653433+mrkprinc@users.noreply.github.com>
2018-10-13 21:43:52 -0400

    card shuffle

--- src/components/CardSide.js
+++ src/components/CardSide.js
@@ -4,7 +4,7 @@ import images from "../images.json";
 const CardSide = props => {
   return(
     <div className={`card-${props.side} center`}>
-      <img src={images[props.imgId].src} alt={images[props.imgId].text} />
+      <img src={images[props.imgId].src} alt={images[props.imgId].text} data-imgid={props.imgId} />
     </div>
   )
 }
